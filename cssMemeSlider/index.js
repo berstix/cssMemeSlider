5915715aa003761bd39869eff6ec044f262df955
Stanislav Bereznyakov <berstix@gmail.com>
2024-09-16 00:11:00 +0400

    init: end desktop cssMemeSlider-task (Mon, Sep 16, 2024 00:10:24 AM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -1,6 +1,7 @@
 console.log('script ready')
-const slider = document.getElementById('slider');
-const sliderBox =document.getElementById('box')
+const slides = document.getElementById('box');
+const sliderBox = document.getElementById('box');
+const markers = document.querySelectorAll('.marker');
 fetch('./index.json')
   .then(response => response.json())
   .then(data => {
@@ -9,7 +10,7 @@ fetch('./index.json')
       sliderBox.appendChild(productCard);
 
       
-      createCardModal(card);
+      
     });
     
   })
@@ -23,9 +24,9 @@ fetch('./index.json')
     card.innerHTML = `
     
      
-      <img src="${data.img}" alt="${data.name}" class="card-img">
-      <h3 class="card-title">${data.text}</h3>
-      <input type="button" value="Learn more" class = "card-button" id="card-btn">
+      <img src="${data.img}" alt="${data.text}" class="mem-img">
+      <h3 class="box-text">${data.text}</h3>
+      
       
       
     `;
@@ -34,3 +35,17 @@ fetch('./index.json')
     return card;
   
   }
+
+  function changeSlide(index) {
+    slides.style.transform = `translateX(-${index * 100}%)`;
+    markers.forEach(marker => marker.classList.remove('active'));
+    markers[index].classList.add('active');
+  }
+  
+  // Добавляем обработчик событий для каждого маркера
+  markers.forEach((marker, index) => {
+    marker.addEventListener('click', () => changeSlide(index));
+  });
+  
+  // Инициализируем первый активный маркер
+  markers[0].classList.add('active');
